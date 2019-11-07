a319f7443c0e42b8d47e4cc6355a088c9c2b004b
Pablo <pablo.gutierrezmartinez@nervion.salesianas.org>
2019-11-07 18:51:48 +0100

    Done: Ej1, Ej2, Ej3, Ej4, Ej5, Ej6. In process: Ej7 (5-fix .this).

--- Ejercicios Clases y Objetos/src/ejercicio7/Linea.java
+++ Ejercicios Clases y Objetos/src/ejercicio7/Linea.java
@@ -5,11 +5,28 @@ public class Linea {
 	private Punto p2;
 	
 	public Linea(Punto p1, Punto p2) {
-		p1=new Punto(2,6);
-		p2=new Punto(0,0);
-	} 
+		this.p1=new Punto(2,6);
+		this.p2=new Punto(0,0);
+	}
+	
+	public Punto getP1() {
+		return p1;
+	}
+
+	public void setP1(Punto p1) {
+		this.p1 = p1;
+	}
+
+	public Punto getP2() {
+		return p2;
+	}
+
+	public void setP2(Punto p2) {
+		this.p2 = p2;
+	}
+
 	public String getPosicion() {
-		 return "[puntoA,puntoB] -> [("+p1;
+		 return "Esto: "+p1.getX();
 	 }
 }
 
