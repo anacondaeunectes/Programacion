6726fe6b2963beab530d92b0fb9170259d419c4a
anacondaeunectes <peiboligutierrez@gmail.com>
2019-11-19 14:22:38 +0100

    m

--- I.E. 2.1. (Boletín 2)/src/main/Main_Ej8_Test.java
+++ I.E. 2.1. (Boletín 2)/src/main/Main_Ej8_Test.java
@@ -5,15 +5,17 @@ import ej8_Alumno.Alumno;
 public class Main_Ej8_Test {
 
 	public static void main(String[] args) {
-		Alumno al1=new Alumno("Guillermo", 15, "Lengua", 8.5);
-		Alumno al2=new Alumno("Matias", 16, "Matematicas", 7);
-		Alumno al3=new Alumno("Miguel", 12, "Educacion Fisica", 6);
+		Alumno alumno1=new Alumno("Guillermo", 15, "Lengua", 8.5);
+		Alumno alumno2=new Alumno("Matias", 16, "Matematicas", 7);
+		Alumno alumno3=new Alumno("Miguel", 12, "Educacion Fisica", 6);
 		
-		System.out.println(al1.getDatosAlumnos());
+		System.out.println(alumno1.getDatosAlumnos());
 		
-		al1.getAsignatura().setNota(3);
+		//De esta forma accedo a cambiar la nota del objeto al que se ha asignado al atributo "asignatura" del 
+		//objeto "alumno1"
+		alumno1.getAsignatura().setNota(3);		
 		
-		System.out.println(al1.getDatosAlumnos());
+		System.out.println(alumno1.getDatosAlumnos());
 		
 		
 	}
